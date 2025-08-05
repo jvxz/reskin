a19106eaa79f8b328d7dcd75ab2f943bcebfdadf
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-08-05 17:35:22 -0400

    feat: add cn utility, global ui styles, variants file

---
+++ app/utils/styles.ts
@@ -0,0 +1,48 @@
+const interactiveStyles = {
+  base: 'cursor-pointer font-medium focus-visible:border-primary/50 select-none focus-visible:ring-ring/40 focus-visible:ring-[3px] active:ring-ring/60 aria-invalid:ring-destructive/20 aria-invalid:border-destructive dark:aria-invalid:ring-destructive/40 shrink-0 gap-2 rounded text-base whitespace-nowrap outline-none disabled:pointer-events-none hover:disabled:cursor-not-allowed disabled:opacity-50 underline-offset-4 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg]:size-4 active:scale-[97%] transition-transform duration-150',
+  size: {
+    default: 'h-8 px-3 py-1.5 text-sm',
+    icon: 'aspect-square size-8',
+    lg: 'h-10 px-5 text-base text-lg',
+    sm: 'h-7 px-2.5 py-1 text-xs',
+  },
+  variant: {
+    default:
+      'bg-primary/85 border hover:bg-primary/90 border-primary active:bg-primary/85 text-primary-foreground glow-primary',
+    destructive:
+      'hover:bg-destructive/90 bg-destructive/90 text-destructive-foreground hover:bg-destructive active:bg-destructive',
+    ghost: 'hover:bg-muted/90 active:bg-muted/80',
+    link: 'text-primary underline-offset-4 hover:underline',
+    outline:
+      'border-border/90 hover:bg-muted/90 active:bg-muted/80 hover:border-border active:border-border border bg-transparent',
+  },
+}
+
+const staticStyles = {
+  base: 'rounded p-5',
+  variant: {
+    default:
+      'bg-card shadow-[0px_-1px_0px_0px_hsl(from_var(--muted-foreground)_h_s_l/0.40),0px_1px_0px_0px_hsl(from_var(--muted)_h_s_l),inset_0px_-2px_0px_0px_hsl(from_var(--muted-foreground)_h_s_l/0.035)] border border-y-0 text-card-foreground',
+    destructive:
+      'bg-card border-destructive text-destructive border [&>svg]:text-current',
+  },
+}
+
+const popoverStyles = {
+  content: [
+    staticStyles.base,
+    staticStyles.variant.default,
+    'data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 min-w-[8rem] overflow-hidden p-1',
+  ],
+  item: [
+    interactiveStyles.base,
+    interactiveStyles.variant.ghost,
+    interactiveStyles.size.default,
+    'focus:bg-muted focus:text-accent-foreground [&_svg:not([class*=\'text-\'])]:text-muted-foreground [&_svg:not([class*=\'size-\'])]:size-4 data-[variant=destructive]:text-destructive-foreground data-[variant=destructive]:focus:bg-destructive/10 data-[variant=destructive]:focus:text-destructive-foreground data-[variant=destructive]:*:[svg]:!text-destructive-foreground dark:data-[variant=destructive]:focus:bg-destructive/40 relative flex cursor-default items-center p-1 px-2 text-sm outline-hidden transition-all select-none focus-visible:ring-0 data-[disabled]:pointer-events-none data-[disabled]:opacity-50 data-[inset]:pl-8 [&_svg]:pointer-events-none [&_svg]:shrink-0',
+  ],
+}
+
+const overlayStyles
+  = 'data-[state=open]:animate-in data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-overlay backdrop-blur-xs'
+
+export { interactiveStyles, overlayStyles, popoverStyles, staticStyles }
---
+++ app/utils/variants.ts
@@ -0,0 +1,26 @@
+import { cva } from 'class-variance-authority'
+
+export const buttonVariants = cva(
+  `${interactiveStyles.base} relative inline-flex items-center justify-center`,
+  {
+    defaultVariants: {
+      size: 'default',
+      variant: 'default',
+    },
+    variants: {
+      size: {
+        default: interactiveStyles.size.default,
+        icon: interactiveStyles.size.icon,
+        lg: interactiveStyles.size.lg,
+        sm: interactiveStyles.size.sm,
+      },
+      variant: {
+        default: interactiveStyles.variant.default,
+        destructive: interactiveStyles.variant.destructive,
+        ghost: interactiveStyles.variant.ghost,
+        link: interactiveStyles.variant.link,
+        outline: interactiveStyles.variant.outline,
+      },
+    },
+  },
+)
