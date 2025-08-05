0f34a835b7ef855722241ed93a5152f16c65642a
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-08-05 18:24:53 -0400

    feat: input component

---
+++ app/components/u/input.vue
@@ -0,0 +1,5 @@
+<template>
+  <input
+    :class="cn(staticStyles.base, interactiveStyles.size.default, staticStyles.variant.default, 'flex w-full min-w-0 cursor-text px-3 py-1 transition-all selection:bg-primary selection:text-primary-foreground file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:ring-0 md:text-sm')"
+  >
+</template>
