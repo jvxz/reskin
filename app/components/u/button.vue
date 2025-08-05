22f009b1f441263ce1b157e4d398e401432e3f86
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-08-05 17:35:29 -0400

    feat: button component

---
+++ app/components/u/button.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+import { Slot } from 'reka-ui'
+
+interface ButtonProps {
+  asChild?: boolean
+  disabled?: boolean
+  size?: 'default' | 'icon' | 'lg' | 'sm'
+  variant?: 'default' | 'destructive' | 'ghost' | 'link' | 'outline'
+}
+
+withDefaults(
+  defineProps<ButtonProps>(),
+  {
+    asChild: false,
+    disabled: false,
+    size: 'default',
+    variant: 'default',
+  },
+)
+</script>
+
+<template>
+  <Slot
+    v-if="asChild"
+    :disabled="disabled"
+    :class="buttonVariants({ variant, size })"
+  >
+    <slot />
+  </Slot>
+  <button
+    v-else
+    :disabled="disabled"
+    :class="buttonVariants({ variant, size })"
+  >
+    <slot />
+  </button>
+</template>
