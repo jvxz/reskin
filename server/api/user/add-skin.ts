00283197a6ccd78c0e6d315cf7ed4f9812bf1e06
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-08-07 17:36:07 -0400

    refactor: rename post-skin endpoint to add-skin, add implementation

---
+++ server/api/user/add-skin.ts
@@ -0,0 +1,35 @@
+import z from 'zod'
+import { skins } from '~~/server/db/schema'
+import { defineAuthedEventHandler } from '~~/server/handlers/auth'
+import { getFileFromB64 } from '~~/server/utils/get-file-from-base64'
+
+export default defineAuthedEventHandler(async (event) => {
+  const { thumbnailBase64, ...skinData } = await readValidatedBody(event, LocalSkinSchema.extend({
+    thumbnailBase64: z.string(),
+  }).parse)
+
+  const [thumbnailFile, skinFile] = await Promise.all([
+    getFileFromB64(thumbnailBase64, `thumbnail_${skinData.id}.png`, 'image/png'),
+    getFileFromB64(skinData.base64, `skin_${skinData.id}.png`, 'image/png'),
+  ])
+
+  const [thumbnailUrl, skinUrl] = await event.context.ut.uploadFiles([thumbnailFile, skinFile])
+
+  if (thumbnailUrl.error || skinUrl.error) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: thumbnailUrl.error?.message ?? skinUrl.error?.message ?? 'An unknown error occurred when uploading your skin',
+    })
+  }
+
+  const skin = {
+    ...skinData,
+    skinUrl: skinUrl.data?.ufsUrl,
+    thumbnailUrl: thumbnailUrl.data?.ufsUrl,
+    userId: event.context.authData.user.id,
+  }
+
+  await event.context.db.insert(skins).values(skin)
+
+  return skin
+})
--- server/api/user/post-skin.ts
+++
@@ -1,5 +0,0 @@
-import { defineAuthedEventHandler } from '~~/server/handlers/auth'
-
-export default defineAuthedEventHandler(async ({ context }) => {
-  return context.authData.user.id
-})
