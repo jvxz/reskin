120984f84879fbaf3bf1e56048552d17f394553d
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-08-06 20:37:58 -0400

    fix: fix input component

--- app/components/u/input.vue
+++ app/components/u/input.vue
@@ -1,5 +1,26 @@
+<script lang="ts" setup>
+import type { HTMLAttributes } from 'vue';
+import { useVModel } from '@vueuse/core';
+
+const props = defineProps<{
+  defaultValue?: string | number
+  modelValue?: string | number
+  class?: HTMLAttributes['class']
+}>()
+
+const emits = defineEmits<{
+  (e: 'update:modelValue', payload: string | number): void
+}>()
+
+const modelValue = useVModel(props, 'modelValue', emits, {
+  defaultValue: props.defaultValue,
+  passive: true,
+})
+</script>
+
 <template>
   <input
+    v-model="modelValue"
     :class="cn(staticStyles.base, interactiveStyles.size.default, staticStyles.variant.default, 'flex w-full min-w-0 cursor-text px-3 py-1 transition-all selection:bg-primary selection:text-primary-foreground file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:ring-0 md:text-sm')"
   >
 </template>
