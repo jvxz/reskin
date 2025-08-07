edd537881ad109863b1e0b55f11949d1127dc407
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-08-07 17:33:45 -0400

    feat: add generate thumbnail utility

---
+++ app/utils/generate-thumbnail.ts
@@ -0,0 +1,48 @@
+import { Buffer } from 'node:buffer'
+import { SkinViewer } from 'skinview3d'
+import { WalkAnimation } from '~/lib/walk-animation'
+import { loadViewerSettings } from './load-canvas-settings'
+
+const WIDTH = 89.6 * 6
+const HEIGHT = 160 * 6
+
+export async function generateThumbnail(
+  skin: LocalSkin,
+) {
+  const canvas = document.createElement('canvas')
+  document.body.appendChild(canvas)
+
+  canvas.width = WIDTH
+  canvas.height = HEIGHT
+
+  const viewer = new SkinViewer({
+    animation: new WalkAnimation(),
+    canvas,
+    height: HEIGHT,
+    width: WIDTH,
+  })
+
+  loadViewerSettings(viewer)
+
+  await viewer.loadSkin(`data:image/png;base64,${skin.base64}`)
+
+  viewer.render()
+
+  const blob = await new Promise<Blob>((resolve, reject) => {
+    requestAnimationFrame(() => {
+      canvas.toBlob((blob) => {
+        if (blob) {
+          resolve(blob)
+        }
+        else {
+          reject(new Error('Failed to generate thumbnail'))
+        }
+      })
+    })
+  })
+
+  const buffer = await blob.arrayBuffer()
+  const base64 = Buffer.from(buffer).toString('base64')
+
+  return base64
+}
