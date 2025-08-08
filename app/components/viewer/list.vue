9a2250d5a447ce33804d19dc66aa5d7ccb9bf4ed
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-08-08 16:51:35 -0400

    feat: add viewer list component

---
+++ app/components/viewer/list.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const { skins } = useSkins()
+</script>
+
+<template>
+  <div class="m-3 w-1/4 space-y-3">
+    <ViewerListHeader />
+    <ViewerListCard
+      v-for="skin in skins"
+      :key="skin.id"
+      :skin="skin"
+    />
+  </div>
+</template>
