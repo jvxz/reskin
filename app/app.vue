c56a539c083ec2fe1bd5a90f3da2e66b515f43d8
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-08-08 16:48:48 -0400

    chore: add pinia colada devtools

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,10 @@
+<script lang="ts" setup>
+import { PiniaColadaDevtools } from '@pinia/colada-devtools';
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
+  <PiniaColadaDevtools />
 </template>
