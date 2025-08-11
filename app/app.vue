f27b59dfa0e9c97c51bdd8c64cb62ca1914a3402
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-08-10 20:35:47 -0400

    refactor: move toaster to app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,7 @@
 <script lang="ts" setup>
-import { PiniaColadaDevtools } from '@pinia/colada-devtools';
+import { PiniaColadaDevtools } from "@pinia/colada-devtools";
+
+type Theme = "light" | "dark" | "system";
 </script>
 
 <template>
@@ -7,4 +9,17 @@ import { PiniaColadaDevtools } from '@pinia/colada-devtools';
     <NuxtPage />
   </NuxtLayout>
   <PiniaColadaDevtools />
+  <Toaster
+    :toast-options="{
+      classes: {
+        actionButton: '!bg-primary !text-primary-foreground font-medium',
+        cancelButton: '!bg-muted !text-muted-foreground font-medium',
+        description: '!text-muted-foreground',
+        toast:
+          'group toast !bg-card !text-card-foreground !border-border !shadow-none',
+      },
+    }"
+    :theme="$colorMode.value as Theme"
+    position="top-right"
+  />
 </template>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,22 +1,5 @@
-<script lang="ts" setup>
-type Theme = 'light' | 'dark' | 'system'
-</script>
-
 <template>
   <div class="h-screen">
     <slot />
-    <Toaster
-      :toast-options="{
-        classes: {
-          actionButton: '!bg-primary !text-primary-foreground font-medium',
-          cancelButton: '!bg-muted !text-muted-foreground font-medium',
-          description: '!text-muted-foreground',
-          toast:
-            'group toast !bg-card !text-card-foreground !border-border !shadow-none',
-        },
-      }"
-      :theme="$colorMode.value as Theme"
-      position="top-right"
-    />
   </div>
 </template>
