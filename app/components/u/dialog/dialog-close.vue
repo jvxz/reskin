ebc5eadee9beee2643323be589062fb3b09a2080
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-08-12 18:30:29 -0400

    feat: add dialog components

---
+++ app/components/u/dialog/dialog-close.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { DialogClose } from "reka-ui"
+
+const props = defineProps<DialogCloseProps>()
+</script>
+
+<template>
+  <DialogClose v-bind="props">
+    <slot />
+  </DialogClose>
+</template>
