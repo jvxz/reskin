5d946702ef9a00d3d7878bf3d97eb8b7d2a213f8
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-08-12 17:32:31 -0400

    feat: add auth button to inset buttons

--- app/components/inset/buttons.vue
+++ app/components/inset/buttons.vue
@@ -1,3 +1,9 @@
+<script lang="ts" setup>
+const { signIn, signOut } = authClient
+
+const { data: session } = await authClient.useSession(useFetch)
+</script>
+
 <template>
   <div class="flex flex-1 flex-col gap-4">
     <UButton
@@ -6,6 +12,39 @@
       class="group size-16"
     >
       <Icon name="tabler:box-model" class="!size-9 text-muted-foreground group-hover:text-foreground" />
+    </UButton>
+    <UButton
+      v-if="!session?.user"
+      size="icon"
+      variant="ghost"
+      class="group size-16"
+      @click="signIn.social({
+        provider: 'github',
+      })"
+    >
+      <Icon name="tabler:login-2" class="!size-9 text-muted-foreground group-hover:text-foreground" />
+    </UButton>
+    <UButton
+      v-else-if="session?.user"
+      size="icon"
+      variant="ghost"
+      class="group size-16"
+      @click="signOut({
+        fetchOptions: {
+          onResponse: () => reloadNuxtApp(),
+        },
+      })"
+    >
+      <Icon name="tabler:logout" class="!size-9 text-muted-foreground group-hover:text-foreground" />
+    </UButton>
+    <UButton
+      v-else
+      size="icon"
+      variant="ghost"
+      class="group size-16"
+      disabled
+    >
+      <USpinner />
     </UButton>
   </div>
 </template>
