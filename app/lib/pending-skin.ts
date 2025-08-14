42554bbdde412af56ba21cc310ef42b6760ca9f0
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-08-14 16:39:25 -0400

    feat: pending skin constant

---
+++ app/lib/pending-skin.ts
@@ -0,0 +1,11 @@
+export const PENDING_SKIN: UserSkin = {
+  base64: '',
+  headBase64: '',
+  id: 'pending',
+  name: 'Pending',
+  skinType: 'CLASSIC',
+  skinUrl: '',
+  source: 'FILE_UPLOAD',
+  thumbnailUrl: '',
+  userId: '',
+}
