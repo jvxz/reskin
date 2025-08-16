2040e6a46bbc0126e8d20f016073d2243d288931
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-08-16 15:31:27 -0400

    chore: edit drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'drizzle-kit'
-import { env } from './env'
 
 export default defineConfig({
-  dbCredentials: {
-    host: env.NUXT_DATABASE_URL,
-    url: env.NUXT_DATABASE_URL,
-  },
-  dialect: 'postgresql',
+  // dbCredentials: {
+  //   host: env.NUXT_DATABASE_URL,
+  //   url: env.NUXT_DATABASE_URL,
+  // },
+  dialect: 'sqlite',
   out: './drizzle',
   schema: './server/db/schema.ts',
 })
