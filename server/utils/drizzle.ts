af2b993c63005329a8b012e84ce60a54cd7fe86a
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-08-16 15:31:32 -0400

    feat: useDrizzle server util

---
+++ server/utils/drizzle.ts
@@ -0,0 +1,13 @@
+import { drizzle } from 'drizzle-orm/d1'
+
+import * as schema from '~~/server/db/schema'
+
+export { and, eq, or, sql } from 'drizzle-orm'
+
+export const tables = schema
+
+export function useDrizzle() {
+  return drizzle(hubDatabase(), { schema })
+}
+
+// export type User = typeof schema.user.$inferSelect
